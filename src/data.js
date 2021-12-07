48af9363e563586a033d30b538cede34beebed39
njeri <cnjeri388@gmail.com>
2021-12-07 17:25:40 +0300

    final commit

--- src/data.js
+++ src/data.js
@@ -12,29 +12,29 @@ const data = [
     },
     {
         id:"2",
-        title: "Mount Fuji",
-        location: "Japan",
+        title: "Mombasa",
+        location: "Kenya",
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "./images/mountains.jpg"
+        description:"Mombasa is a coastal city in southeastern Kenya along the Indian Ocean. The city is known as the white and blue city in Kenya. It is the country's oldest and second-largest city, with a population of about 1,208,333 people according to the 2019 census",
+        imageUrl: "./images/mombasa.jpg"
     },
     {
         id:"3",
-        title: "Mount Fuji",
-        location: "Japan",
+        title: "Mount Kenya",
+        location: "Kenya",
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "./images/Nanyuki.jpg"
+        startDate: "12 Jan, 2012",
+        endDate: "24 Jan, 2012",
+        description: "Mount Kenya is the highest mountain in Kenya and the second-highest in Africa, after Kilimanjaro. The highest peaks of the mountain are Batian, Nelion and Point Lenana.",
+        imageUrl: "./images/Mtkenya.jpg"
     },
     {
         id:"4",
         title: "Samburu",
         location: "Kenya",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        googleMapsUrl: "https://www.google.com/maps/place/Samburu+County",
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
         description: "Samburu County is the northerly gem globally reputed and associated with Born Free film, Camel racing,breathtaking tourism sites.",
